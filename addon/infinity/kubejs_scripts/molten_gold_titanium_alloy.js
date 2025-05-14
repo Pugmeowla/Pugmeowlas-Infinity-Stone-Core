ca6435d3dea7db31800e7a42645c63e3830e4ea5
Pugmeowla <andretnk49@hotmail.com>
2025-05-14 22:51:30 +1000

    sdgbkjbsjkls

---
+++ addon/infinity/kubejs_scripts/molten_gold_titanium_alloy.js
@@ -0,0 +1,6 @@
+StartupEvents.registry('fluid', event => {
+  event.create('molten_gold_titanium_alloy')
+    .thickTexture(0x635C1B)
+    .bucketColor(0x635C1B)
+    .displayName('Molten Gold-Titanium Alloy')
+    })
