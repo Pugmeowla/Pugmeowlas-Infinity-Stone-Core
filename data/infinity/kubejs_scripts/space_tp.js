67aef038b91f36e4d798087cb8cb6f0854bcb4b1
Pugmeowla <andretnk49@hotmail.com>
2025-06-27 22:10:35 +1000

    tp

---
+++ data/infinity/kubejs_scripts/space_tp.js
@@ -0,0 +1,30 @@
+ServerEvents.commandRegistry(event => {
+    const { commands: Commands, arguments: Arguments } = event
+    event.register(
+        Commands.literal("space_tp")
+            .then(Commands.argument('x', Arguments.STRING.create(event))
+                .then(Commands.argument('y', Arguments.STRING.create(event))
+                    .then(Commands.argument('z', Arguments.STRING.create(event))
+                        .then(Commands.argument('dimension', Arguments.RESOURCE_LOCATION.create(event))
+                        .executes(ctx => {
+                            let x = Arguments.STRING.getResult(ctx, "x")
+                            let y = Arguments.STRING.getResult(ctx, "y")
+                            let z = Arguments.STRING.getResult(ctx, "z")
+                            let dimension = Arguments.RESOURCE_LOCATION.getResult(ctx, "dimension")
+
+                            let server = ctx.source.getServer()
+                            let player = ctx.source.player
+                            let dim = player.getLevel().getDimension()
+
+                            let username = player.getGameProfile().getName();
+
+                            if (abilityUtil.isEnabled(player, 'infinity:space_stone', 'Teleport')) {
+                                server.runCommandSilent(`execute as ${username} at @s in ${dim} as @e[distance=..3] run execute in ${dimension} run tp @s ${x} ${y} ${z}`)
+                            
+                }
+            
+                return 1;
+            })
+            
+    ))) ));
+})
