8214cffc4535b7d7633c8d97cf0d349150b63e7e
Pugmeowla <andretnk49@hotmail.com>
2025-12-14 17:14:25 +1100

    a frickin ton

---
+++ assets/infinity/kubejs_scripts/t-pose2.js
@@ -0,0 +1,71 @@
+PalladiumEvents.registerAnimations((event) => {
+    event.register('infinity/tposeactivation2', 200, (builder) => {
+
+        // Check the Chronosapien T-pose ability
+        let activation = animationUtil.getAnimationTimerAbilityValue(
+            builder.getPlayer(), 'infinity:infinity_gauntlet', 't-pose', builder.getPartialTicks()
+        );
+
+        if (activation > 0 && !builder.isFirstPerson()) {
+            // Third-person T-pose
+            builder.get('right_arm')
+                .setXRotDegrees(-25)
+                .setYRotDegrees(0)
+                .setZRotDegrees(115)
+                .setX(-6)
+                .setY(3)
+                .setZ(1)
+                .animate('easeOutBack', activation);
+
+            builder.get('left_arm')
+                .setXRotDegrees(-25)
+                .setYRotDegrees(0)
+                .setZRotDegrees(-115)
+                .setX(6)
+                .setY(3)
+                .setZ(1)
+                .animate('easeOutBack', activation);
+
+                 builder.get('left_leg')
+                .setXRotDegrees(-10)
+                .setYRotDegrees(-45)
+                .setZRotDegrees(0)
+                .animate('easeOutBack', activation);
+
+                 builder.get('right_leg')
+                .setXRotDegrees(-10)
+                .setYRotDegrees(45)
+                .setZRotDegrees(0)
+                .animate('easeOutBack', activation);
+
+
+            // Force head forward
+           
+
+        }
+
+        if (activation > 0 && builder.isFirstPerson()) {
+            // First-person T-pose
+            builder.get('right_arm')
+                .setXRotDegrees(0)
+                .setYRotDegrees(0)
+                .setZRotDegrees(50)
+                .setX(-8)
+                .setY(6)
+                .setZ(4)
+                .animate('easeOutBack', activation);
+
+            builder.get('left_arm')
+                .setXRotDegrees(0)
+                .setYRotDegrees(0)
+                .setZRotDegrees(-50)
+                .setX(8)
+                .setY(6)
+                .setZ(4)
+                .animate('easeOutBack', activation);
+
+            // Force head forward
+          
+        }
+    });
+});
