db2e2ed3c4db4778086e9f39468172c17752d723
Pugmeowla <andretnk49@hotmail.com>
2026-01-22 15:11:55 +1100

    ferro stuff

---
+++ addon/infinity/kubejs_scripts/molten_ferro-titanium-gold_alloy.js
@@ -0,0 +1,6 @@
+StartupEvents.registry('fluid', event => {
+  event.create('molten_ferro-titanium-gold_alloy')
+    .thickTexture(0x404040)
+    .bucketColor(0x404040)
+    .displayName('Molten Ferro-Titanium-Gold Alloy')
+    })
